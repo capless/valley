227a0310c1f62a4decc643e019c6eb00616c8424
Brian Jinwright <bjinwright@qwigo.com>
2017-03-29 23:25:35 -0400

    added url to setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from pip.req import parse_requirements
 
 install_reqs = parse_requirements('requirements.txt', session=False)
 
-version = '1.0.0'
+version = '1.0.1'
 
 LONG_DESCRIPTION = """
 =======================
@@ -11,6 +11,7 @@ Valley
 =======================
 
 Python extensible schema validations and declarative syntax helpers.
+
 
 """
 
@@ -29,6 +30,7 @@ setup(
     author_email='opensource@capless.io',
     maintainer='Brian Jinwright',
     packages=find_packages(),
+    url='https://github.com/capless/valley',
     license='GNU GPL V3',
     install_requires=[str(ir.req) for ir in install_reqs],
     include_package_data=True,
