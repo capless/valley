e3811fa1694113c4e02316e49780a4e7b53d005b
Brian Jinwright <bjinwright@qwigo.com>
2017-04-02 20:30:02 -0400

    added more tests, added OrderedDeclaredVars class

--- valley/declarative.py
+++ valley/declarative.py
@@ -1,33 +1,52 @@
+import collections
+
 class DeclaredVars(object):
 
     base_field_class = None
 
-    def get_declared_variables(self,bases, attrs):
+    def get_base_fields(self,bases, attrs):
         properties = {}
-        f_update = properties.update
+        p_update = properties.update
         attrs_pop = attrs.pop
         for variable_name, obj in list(attrs.items()):
             if isinstance(obj, self.base_field_class):
-                f_update({variable_name: attrs_pop(variable_name)})
+                properties[variable_name] = attrs_pop(variable_name)
 
         for base in bases:
             if hasattr(base, '_base_properties'):
                 bft = base._base_properties
                 if len(bft) > 0:
-                    f_update(bft)
+                    p_update(bft)
         return properties
 
 
+class OrderedDeclaredVars(DeclaredVars):
+
+    def get_base_fields(self,bases, attrs):
+
+        properties = collections.OrderedDict(
+            sorted(
+                filter(lambda x: isinstance(x[1],self.base_field_class),
+                attrs.items()
+                       ),
+                key=lambda x: x[1]._creation_counter))
+
+        p_update = properties.update
+
+        for base in bases:
+            if hasattr(base, '_base_properties'):
+                bft = base._base_properties
+                if len(bft) > 0:
+                    p_update(bft)
+
+        return properties
+
 class DeclarativeVariablesMetaclass(type):
-    """
-    Partially ripped off from Django's forms.
-    http://code.djangoproject.com/browser/django/trunk/django/forms/forms.py
-    """
 
     declared_vars_class = None
 
     def __new__(cls, name, bases, attrs):
-        attrs['_base_properties'] = cls.declared_vars_class().get_declared_variables(bases, attrs)
+        attrs['_base_properties'] = cls.declared_vars_class().get_base_fields(bases, attrs)
         new_class = super(DeclarativeVariablesMetaclass,
                           cls).__new__(cls, name, bases, attrs)
 
